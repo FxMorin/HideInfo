dcaf4125d1181d34d98a4abd8cc986fc9508d7d6
FX - PR0CESS <fx.e.morin@gmail.com>
2021-10-25 13:35:03 -0400

    Initial commit

---
+++ src/main/java/ca/fxco/hideinfo/Commands/CommandHideInfo.java
@@ -0,0 +1,57 @@
+package ca.fxco.hideinfo.Commands;
+
+import ca.fxco.hideinfo.HideInfo;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+public class CommandHideInfo implements CommandExecutor {
+
+    private final HideInfo hideInfo;
+
+    private final String PREFIX = "§8[§dHide§5Info§8] §f";
+
+    public CommandHideInfo(HideInfo hideInfo) {
+        super();
+        this.hideInfo = hideInfo;
+    }
+
+    public void sendHelpMenu(@NotNull CommandSender sender) {
+        sender.sendMessage(" §6=============== §dHide§5Info §fHelp §6===============");
+        for (Map.Entry<String, String> name : this.hideInfo.toggles.entrySet()) {
+            sender.sendMessage(PREFIX+(this.hideInfo.config.getBoolean(name.getKey()) ? "§2" : "§4")+name.getKey()+"§f: "+name.getValue());
+        }
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender.isOp()) {
+            if(args.length == 0) {
+                sendHelpMenu(sender);
+            } else if (args.length <= 2) {
+                if (args[0].equalsIgnoreCase("help")) {
+                    sendHelpMenu(sender);
+                } else {
+                    if (this.hideInfo.toggles.containsKey(args[0].toLowerCase())) {
+                        String value = args[0].toLowerCase();
+                        if (args.length == 2) {
+                            this.hideInfo.config.set(value, args[1].equalsIgnoreCase("true"));
+                        } else {
+                            this.hideInfo.config.set(value, !this.hideInfo.config.getBoolean(value));
+                        }
+                        this.hideInfo.onModifyConfig();
+                        sender.sendMessage(PREFIX + "§9" + value + "§f is now: " + (this.hideInfo.config.getBoolean(value) ? "§2Showing" : "§4Hiding"));
+                    } else {
+                        sender.sendMessage(PREFIX + "§9" + args[0] + "§f is not a valid option!");
+                    }
+                }
+            } else {
+                sender.sendMessage(PREFIX+"Invalid Syntax: /hideinfo <option> [<toggle>]");
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/ca/fxco/hideinfo/HideInfo.java
@@ -0,0 +1,120 @@
+package ca.fxco.hideinfo;
+
+import ca.fxco.hideinfo.Commands.CommandHideInfo;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.*;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+
+public final class HideInfo extends JavaPlugin implements Listener {
+
+    //Plugin made for PhoenixSC & Shoezilla
+
+    public LinkedHashMap<String, String> toggles = new LinkedHashMap<>();
+    public FileConfiguration config = getConfig();
+
+    public HideInfo() {
+        super();
+        initToggles();
+    }
+
+    protected HideInfo(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
+        super(loader, description, dataFolder, file);
+        initToggles();
+    }
+
+    public void initToggles() {
+        toggles.put("leave", "Toggle player leave messages");
+        toggles.put("join", "Toggle player join messages");
+        toggles.put("chat", "Toggle chat");
+        toggles.put("me", "Toggle /me");
+        toggles.put("msg", "Toggle /msg, /tell, & /w");
+        toggles.put("teammsg", "Toggle /teammsg, & /tm");
+        toggles.put("plugins", "Toggle /plugins, /pl, & /?");
+        toggles.put("vanilla", "Toggle /minecraft:<cmd>");
+        toggles.put("secure", "Toggle /<namespace>:<cmd>");
+        toggles.put("help", "Toggle /help");
+    }
+
+    @Override
+    public void onEnable() {
+        for (String name : toggles.keySet()) {
+            config.addDefault(name, true);
+        }
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        getServer().getPluginManager().registerEvents(this, this);
+        this.getCommand("hideinfo").setExecutor(new CommandHideInfo(this));
+        getLogger().info("[HideInfo] FX was here!");
+    }
+
+    public void onModifyConfig() {
+        saveConfig();
+    }
+
+    @Override
+    public void onDisable() {}
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (!config.getBoolean("join")) {
+            event.setJoinMessage("");
+        }
+    }
+
+    @EventHandler
+    public void onPlayerChat(PlayerChatEvent event) {
+        if (config.getBoolean("chat") || !event.getPlayer().isOp()) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerChatAsync(AsyncPlayerChatEvent event) {
+        if (config.getBoolean("chat") || !event.getPlayer().isOp()) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerChatTab(PlayerChatTabCompleteEvent event) {
+        if (event.getPlayer().isOp() && event.getChatMessage().startsWith("/hideinfo")) {
+            event.getTabCompletions().clear();
+            event.getTabCompletions().addAll(toggles.keySet());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        if (!config.getBoolean("leave")) {
+            event.setQuitMessage("");
+        }
+    }
+
+    @EventHandler
+    public void onCommandPreProcess(PlayerCommandPreprocessEvent event) {
+        String msg = event.getMessage().toLowerCase();
+        if (!config.getBoolean("secure") && msg.contains(":")) {
+            event.setCancelled(true);
+        } else if (!config.getBoolean("vanilla") && msg.startsWith("/minecraft:")) {
+            event.setCancelled(true);
+        } else if (!config.getBoolean("msg") && (msg.startsWith("/tell") || msg.startsWith("/msg") || msg.startsWith("/w"))) {
+            event.setCancelled(true);
+        } else if (!config.getBoolean("me") && msg.startsWith("/me")) {
+            event.setCancelled(true);
+        } else if (!config.getBoolean("teammsg") && (msg.startsWith("/teammsg") || msg.startsWith("/tm"))) {
+            event.setCancelled(true);
+        } else if (!config.getBoolean("plugins") && (msg.startsWith("/plugins") || msg.startsWith("/pl") || msg.startsWith("/?"))) {
+            event.setCancelled(true);
+        } else if (!config.getBoolean("help") && msg.startsWith("/help")) {
+            event.setCancelled(true);
+        }
+    }
+}
